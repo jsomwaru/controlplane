892335f36f94132bbab9b7f96bca930366a8dec1
Jacob Somwaru <jacobsomwaru@gmail.com>
2025-02-14 17:05:43 -0500

    clean up

--- internal/controller/terraformrequest_controller.go
+++ internal/controller/terraformrequest_controller.go
@@ -48,21 +48,12 @@ type TerraformRequestReconciler struct {
 // +kubebuilder:rbac:groups=controlplane.io.controlplane.io,resources=terraformrequests/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=controlplane.io.controlplane.io,resources=terraformrequests/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the TerraformRequest object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.0/pkg/reconcile
 func (r *TerraformRequestReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
 	tfreq := &controlplaneiov1.TerraformRequest{}
 	if err := r.Get(ctx, req.NamespacedName, tfreq); err != nil {
-		return ctrl.Result{Requeue: true, RequeueAfter: time.Second * time.Duration(waitTime)}, nil
+		return ctrl.Result{RequeueAfter: time.Second * time.Duration(waitTime)}, nil
 	}
 
 	log.Info("tfreq status", "status", tfreq.Status.Applied)
